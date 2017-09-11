a9656315d0d7e74c347efa34896dcbca38cb4063
Gustavo Coronel <gcoronelc@gmail.com>
2017-09-11 17:20:30 -0500

    Sem 03

---
+++ Sem03/DistanciaRecta/src/pe/ucv/distanciarecta/DistanciaRecta.java
@@ -0,0 +1,11 @@
+package pe.ucv.distanciarecta;
+
+import pe.ucv.app.view.DistanciaView;
+
+public class DistanciaRecta {
+
+  public static void main(String[] args) {
+    DistanciaView.main(args);
+  }
+  
+}
---
+++ Sem03/PagoApp/src/pe/ucv/pagoapp/PagoApp.java
@@ -0,0 +1,13 @@
+
+package pe.ucv.pagoapp;
+
+import pe.ucv.pagoapp.view.PagoView;
+
+
+public class PagoApp {
+
+  public static void main(String[] args) {
+    PagoView.main(args);
+  }
+  
+}
